105370920a18424e42f10a4cd78457412b6c968c
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-10-19 14:32:22 -0400

    Improved datatable functionality with Show Map and Email buttons 10-19-16

--- src/main/webapp/js/formatMatchResults.js
+++ src/main/webapp/js/formatMatchResults.js
@@ -33,9 +33,18 @@ $(function(){
     
     });
      
-    $('#matchResult tbody').on( 'click', 'tr', function () {
+    $('#matchResult tbody').on( 'click', '.mapButton', function () {
         
-        var data = table.row( this ).data();
+        var data = table.row( $(this).closest("tr") ).data();
+        
+        setTimeout(function(){
+        	 window.open("https://www.google.com/maps/dir/" + data[5] +"/" + data[10] + "/" + data[11] + "/"+ data[7] +"/", "mapWindow", "width=1200,height=900");
+        	}, 500);
+    } );
+    
+    $('#matchResult tbody').on( 'click', '.emailButton', function () {
+        
+        var data = table.row( $(this).closest("tr") ).data();
         $("#name").val(data[1]);
         $("#email").val(data[4]);
         $("#cellPhone").val(data[3]);
@@ -44,9 +53,8 @@ $(function(){
         $("#userHomeAddress").val(data[10]);
         $("#userWorkAddress").val(data[11]);
         
-        setTimeout(function(){
-        	 window.open("https://www.google.com/maps/dir/" + data[5] +"/" + data[10] + "/" + data[11] + "/"+ data[7] +"/", "mapWindow", "width=1200,height=900");
-        	}, 500);
+        $(document).scrollTop( $("#contactForm").offset().top);
+        
     } );
     
 })
